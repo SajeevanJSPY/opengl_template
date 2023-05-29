d4036e7b634c021c95824e32f1618acb269f7048
Sajeevan <sajeevanjspy@gmail.com>
2023-05-29 11:36:33 +0530

    setup: initial

---
+++ README.md
@@ -0,0 +1,14 @@
+# Opengl Template for Projects
+
+This repo only for my personal projects, If you want to use this repo make sure to setup your local machine
+
+**Used Technologies**
+programming language - C++
+build tool - [cmake](https://cmake.org/) 
+graphics api - opengl
+
+**my local machine**
+OS - Ubuntu 22.04.2 LTS x86_64
+
+**setup**
+make sure [glfw](https://www.glfw.org/) installed and setup [glad_lib](https://glad.dav1d.de/) on your local machine
---
+++ utils.h
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+void printShaderLog(unsigned int shader) {
+    int len = 0;
+    int chWrittn = 0;
+    char *log;
+    glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &len);
+
+    if (len > 0) {
+        log = (char *)malloc(len);
+        glGetShaderInfoLog(shader, len, &chWrittn, log);
+        std::cout << "Shader info log: " << log << std::endl;
+        free(log);
+    }
+}
+
+void printProgramLog(int prog) {
+    int len = 0;
+    int chWrittn = 0;
+    char *log;
+    glGetProgramiv(prog, GL_INFO_LOG_LENGTH, &len);
+    if (len > 0) {
+        log = (char *)malloc(len);
+         glGetProgramInfoLog(prog, len, &chWrittn, log);
+         std::cout << "Program Info Log: " << log << std::endl;
+        free(log);
+    } 
+}
+
+bool checkOpenGLError() {
+    bool foundError = false;
+    int glErr = glGetError();
+    while (glErr != GL_NO_ERROR) {
+        std::cout << "glError: " << glErr << std::endl;
+         foundError = true;
+         glErr = glGetError();
+    }
+    return foundError;
+}
+
+const char *vshaderSource = 
+    "#version 450 core\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(0.f, 0.f, 0.f, 1.f);\n"
+    "}\0";
+
+const char *fshaderSource = 
+    "#version 450 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\n\0";
+
+unsigned int createShaderSource() {
+    signed int vertCompiled;
+    signed int fragCompiled;
+    signed int linked;
+
+    unsigned int vShader = glCreateShader(GL_VERTEX_SHADER);
+    unsigned int fShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+    glShaderSource(vShader, 1, &vshaderSource, nullptr);
+    glShaderSource(fShader, 1, &fshaderSource, nullptr);
+    glCompileShader(vShader);
+
+    checkOpenGLError();
+    glGetShaderiv(vShader, GL_COMPILE_STATUS, &vertCompiled);
+    if (vertCompiled != 1) {
+        std::cout << "vertex compilation failed" << std::endl;
+        printShaderLog(vShader);
+    }
+
+    glCompileShader(fShader);
+    checkOpenGLError();
+    glGetShaderiv(vShader, GL_COMPILE_STATUS, &fragCompiled);
+    if (vertCompiled != 1) {
+        std::cout << "fragment compilation failed" << std::endl;
+        printShaderLog(vShader);
+    }
+
+    unsigned int vfProgram = glCreateProgram();
+    glAttachShader(vfProgram, vShader);
+    glAttachShader(vfProgram, fShader);
+    glLinkProgram(vfProgram);
+    checkOpenGLError();
+    glGetProgramiv(vfProgram, GL_LINK_STATUS, &linked);
+    if (linked != 1) {
+        std::cout << "linking failed" << std::endl;
+        printProgramLog(vfProgram);
+    }
+
+    return vfProgram;
+}
+
